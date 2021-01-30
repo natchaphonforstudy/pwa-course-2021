ebade0b988eaa78f7127178c70d60173ea518582
TeerathepS <teerathep.saia@bumail.net>
2021-01-30 10:32:28 +0700

    แก้โ8้ด=นกัน

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-<<<<<<< HEAD
-import logo from './Logo.jpg';
-=======
->>>>>>> 778385579ddf447a65241eb146d59ad73290a5af
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link here
-        </a>
-      </header>
-=======
       <div className="App-header">
       </div>
       <div className="App-chatroom">
@@ -37,7 +17,6 @@ function App() {
         <input type="text" className="App-textbox-input"/>
         <div className="App-textbox-send">Send!</div>
       </div>
->>>>>>> 778385579ddf447a65241eb146d59ad73290a5af
     </div>
   );
 }
